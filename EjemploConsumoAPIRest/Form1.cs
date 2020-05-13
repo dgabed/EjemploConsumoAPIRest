3ff7e19aef5f3e280937155409412bfa7b2abc73
dgabed <dtgabriel@gmail.com>
2020-05-13 13:07:43 -0300

    Agregado y funcionando Api REST Open Weather, y parse de JSON en modo consola

--- EjemploConsumoAPIRest/Form1.cs
+++ EjemploConsumoAPIRest/Form1.cs
@@ -7,14 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
 
 namespace EjemploConsumoAPIRest
 {
     public partial class Form1 : Form
     {
+        String ServicioRest;
+        String URL;
         public Form1()
         {
             InitializeComponent();
+
+            URL = "http://api.openweathermap.org/data/2.5/weather?id=6559994&appid=2535fbd1758d2ba73655d6b6dd5ad383";
+            var json = new WebClient().DownloadString(URL);
+           
+
         }
     }
 }
---
+++ consumoAPIRestConsola/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace consumoAPIRestConsola
+{
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            String url;
+
+            url = "http://api.openweathermap.org/data/2.5/weather?id=6559994&appid=2535fbd1758d2ba73655d6b6dd5ad383";
+            var json = new WebClient().DownloadString(url);
+            dynamic objetoJson = JsonConvert.DeserializeObject(json);
+
+            Console.WriteLine("Json: " + json);
+            Console.ReadLine();
+
+            Console.WriteLine("Nombre: " + objetoJson.name);
+            Console.WriteLine("Latitud: " + objetoJson.coord.lon);
+            Console.WriteLine("Longitud: "+objetoJson.coord.lon);
+            Console.WriteLine("Clima: " + objetoJson.weather[0].description);
+            
+            Console.ReadLine();
+           
+        }
+    }
+}
